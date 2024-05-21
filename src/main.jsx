df393ba3113437bb3b75e6aad275967e48ca2a9e
firsttraining <t9511015@gmail.com>
2024-05-21 02:21:21 -0400

    preloader

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,10 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { Mini } from './mini.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Mini />
   </React.StrictMode>,
 )
